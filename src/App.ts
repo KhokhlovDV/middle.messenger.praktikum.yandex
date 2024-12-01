aa0d3196a86490adbf76d0de33688006e65ec390
Dmitry Khokhlov <d.hohlov@corp.mail.ru>
2024-12-01 15:41:06 +0300

    Handlebars:check working

--- src/App.ts
+++ src/App.ts
@@ -1,9 +1,16 @@
+import Handlebars from 'handlebars';
+import { Button } from './components/button';
+import * as Pages from './pages';
+
+Handlebars.registerPartial('Button', Button);
+
 export class App {
     private appElement: HTMLDivElement;
     constructor() {
         this.appElement = document.getElementById('app') as HTMLDivElement;
     }
     render() {
-        this.appElement.innerHTML = 'Initial setup';
+        const template = Handlebars.compile(Pages.SignIn);
+        this.appElement.innerHTML = template({});
     }
 }
