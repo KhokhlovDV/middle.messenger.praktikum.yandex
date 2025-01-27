a9aa9186fe1f35fb4713859944429a1aca34bec1
Dmitry Khokhlov <d.hohlov@corp.mail.ru>
2025-01-27 23:48:34 +0300

    Mocha:trying...

---
+++ src/utils/http-transport/HttpTransport.test.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+
+describe('Http Transport', () => {
+    it('check', () => {
+        expect(true).to.equal(true);
+    });
+});
