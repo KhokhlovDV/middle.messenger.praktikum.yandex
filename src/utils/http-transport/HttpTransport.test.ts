7f75ab8b432f23337b4728e4f1cb6522421209d7
Dmitry Khokhlov <d.hohlov@corp.mail.ru>
2025-01-28 00:07:27 +0300

    Husky...

--- src/utils/http-transport/HttpTransport.test.ts
+++ src/utils/http-transport/HttpTransport.test.ts
@@ -3,6 +3,6 @@ import { describe, it } from 'mocha';
 
 describe('Http Transport', () => {
     it('check', () => {
-        expect(true).to.equal(true);
+        expect(true).to.equal(false);
     });
 });
