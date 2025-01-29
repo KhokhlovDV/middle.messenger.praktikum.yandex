b8072451c4f655da981b141846ec5220cfa01dd7
Dmitry Khokhlov <d.hohlov@corp.mail.ru>
2025-01-29 23:20:32 +0300

    Some fixes

--- mochaSetup.cjs
+++ mochaSetup.cjs
@@ -5,7 +5,7 @@ const jsdom = new JSDOM('<body><div id="app"></div></body>', {
 });
 global.window = jsdom.window;
 global.document = jsdom.window.document;
-global.FormData = jsdom.FormData;
+global.FormData = jsdom.window.FormData;
 global.DocumentFragment = jsdom.window.DocumentFragment;
 global.Element = jsdom.window.Element;
 global.HTMLElement = jsdom.window.HTMLElement;
